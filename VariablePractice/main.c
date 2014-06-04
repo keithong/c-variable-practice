619b8e869159b0b09ae02f62450a2b963243159a
Keith Samson <k.samson@klab.com>
2014-06-04 10:15:29 +0800

    Variable Practice's first commit

---
+++ VariablePractice/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  VariablePractice
+//
+//  Created by Keith Samson on 6/4/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int x = 19;
+    float y = 1.9;
+    
+    printf("The value of x is %d\n", x);
+    
+    return 0;
+}
+
